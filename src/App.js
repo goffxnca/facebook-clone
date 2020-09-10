e80ff40aaa1416ab5aa7bf4324487d6f65b6f5d0
goffjira <goffjira@gmail.com>
2020-09-10 13:30:35 +0700

    add header section with icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
       <h1>Let's build facebook with React!</h1>
+      <Header />
+      {/* App body */}
+      {/* Sidebar */}
+      {/* Feed */}
+      {/* Widgets */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import FlagIcon from "@material-ui/icons/Flag";
+import SubscriptionOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined";
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
+import { Avatar, IconButton } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import ForumIcon from "@material-ui/icons/Forum";
+import NotificationsActive from "@material-ui/icons/NotificationsActive";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+
+function Header() {
+  return (
+    <div class="header">
+      <div className="header_left">
+        <img
+          src="https://seeklogo.com/images/F/facebook-new-2019-logo-5A4671100B-seeklogo.com.png"
+          alt=""
+        />
+        <div className="header_input">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header_center">
+        <div className="header_option">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <SubscriptionOutlinedIcon fontSize="large" />
+        </div>
+
+        <div className="header_option">
+          <StorefrontOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <SupervisedUserCircleIcon fontSize="large" />
+        </div>
+      </div>
+
+      <div className="header_right">
+        <div className="header_info">
+          <Avatar />
+          <h4>Goff Lee</h4>
+        </div>
+
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+        <IconButton>
+          <ForumIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsActive />
+        </IconButton>
+        <IconButton>
+          <ExpandMore />
+        </IconButton>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
